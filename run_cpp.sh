d6e720cb232387d54c22f61ee693fd8f814d3c1d
ram1123 <ramkrishna.sharma71@gmail.com>
2015-01-20 06:35:50 -0600

    Added macro to compare branches of different trees

--- run_cpp.sh
+++ run_cpp.sh
@@ -4,4 +4,4 @@ c++ -o anaPhantom `root-config --glibs --cflags` anaPhantom.cpp
 echo "Done Compilation."
 echo "./anaPhantom /eos/uscms/store/user/rasharma/lhe_files/POLARIZED_Samples/13TeVSample/ /eos/uscms/store/user/rasharma/LHE_outputs/13TeV/pp_WWJJ_TT/lhe_wplepwmhad_LL.root"
 echo "./anaPhantom /eos/uscms/store/user/rasharma/lhe_files/POLARIZED_Samples/13TeVSample/pp_WWJJ_TOT/unweighted_events_decayed.lhe /eos/uscms/store/user/rasharma/LHE_outputs/13TeV/pp_WWJJ_TOT/lhe.root"
-./anaPhantom /eos/uscms/store/user/rasharma/lhe_files/POLARIZED_Samples/13TeVSample/pp_WWJJ_TOT/unweighted_events_decayed.lhe /eos/uscms/store/user/rasharma/LHE_outputs/13TeV/pp_WWJJ_TOT/lhe.root
+#./anaPhantom /eos/uscms/store/user/rasharma/lhe_files/POLARIZED_Samples/13TeVSample/pp_WWJJ_TOT/unweighted_events_decayed.lhe /eos/uscms/store/user/rasharma/LHE_outputs/13TeV/pp_WWJJ_TOT/lhe.root
