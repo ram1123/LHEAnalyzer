62d68acb6a5f0ee692d66630ad927f9689641f79
ram1123 <ramkrishna.sharma71@gmail.com>
2015-02-16 10:52:23 -0600

    Updated & Changed name of anaPhantom to LHEanalyzer.C

---
+++ LHEanalyzer.h
@@ -0,0 +1,14 @@
+#ifndef LHEANALYZER_H
+#define LHEANALYZER_H
+
+    float mWW, mWLep, mWHad, costheta1, costheta2, costhetastar, phi, phi1;
+    float dEtajj, dPhijj, mjj;
+    int isSignal,isMuMinus;
+    float Lep0_px, 	Lep0_py, 	Lep0_pz, 	Lep0_E  ;
+    float Lep1_px, 	Lep1_py, 	Lep1_pz, 	Lep1_E  ;
+    float Wqrk0_px,	Wqrk0_py, 	Wqrk0_pz, 	Wqrk0_E;
+    float Wqrk1_px, 	Wqrk1_py, 	Wqrk1_pz, 	Wqrk1_E;
+    float Iqrk0_px, 	Iqrk0_py, 	Iqrk0_pz, 	Iqrk0_E;
+    float Iqrk1_px, 	Iqrk1_py, 	Iqrk1_pz, 	Iqrk1_E;
+
+#endif    
