7ab464e901852616b7e4d4a39d1c0bf74c45856a
ram1123 <ramkrishna.sharma71@gmail.com>
2015-02-27 12:36:15 -0600

    run_cpp.sh

--- run_cpp.sh
+++ run_cpp.sh
@@ -1,9 +1,7 @@
 #echo "c++ -o LHEanalyzer `root-config --glibs --cflags` LHEanalyzer.cpp"
 echo "Compiling LHEanalyzer.cpp.................."
-c++ -o LHEanalyzer `root-config --glibs --cflags` LHEanalyzer.cpp
+#c++ -o LHEanalyzer `root-config --glibs --cflags` LHEanalyzer.cpp
+c++ -o LHEanalyzer `root-config --glibs --cflags` LHEanalyzer_ppzz.cpp
 echo "Done Compilation."
-#echo "./LHEanalyzer /eos/uscms/store/user/rasharma/lhe_files/POLARIZED_Samples/13TeVSample/ /eos/uscms/store/user/rasharma/LHE_outputs/13TeV/pp_WWJJ_TT/lhe_wplepwmhad_LL.root"
-#echo "./LHEanalyzer /eos/uscms/store/user/rasharma/lhe_files/POLARIZED_Samples/13TeVSample/pp_WWJJ_TOT/unweighted_events_decayed.lhe /eos/uscms/store/user/rasharma/LHE_outputs/13TeV/pp_WWJJ_TOT/lhe.root"
-##./LHEanalyzer /eos/uscms/store/user/rasharma/lhe_files/POLARIZED_Samples/13TeVSample/pp_WWJJ_TOT/unweighted_events_decayed.lhe /eos/uscms/store/user/rasharma/LHE_outputs/13TeV/pp_WWJJ_TOT/lhe.root
-##./LHEanalyzer /eos/usqcms/store/user/rasharma/lhe_files/POLARIZED_Samples/13TeVSample/unweighted_events.lhe TT_wphad_wmlep.root
-./LHEanalyzer /eos/uscms/store/user/rasharma/lhe_files/POLARIZED_Samples/13TeVSample/pp_WWJJ_LL/NoPtEtaCut/unweighted_events_decayed_wpLepwmHad.lhe /eos/uscms/store/user/rasharma/WlWl_wpHadwmLep_NoptEtaCut.root
+#./LHEanalyzer /eos/uscms/store/user/rasharma/lhe_files/POLARIZED_Samples/13TeVSample/pp_WWJJ_LL/NoPtEtaCut/unweighted_events_decayed_wpLepwmHad.lhe /eos/uscms/store/user/rasharma/WlWl_wpHadwmLep_NoptEtaCut.root
+./LHEanalyzer /eos/uscms/store/user/rasharma/lhe_files/unweighted_events_decayed.lhe pp_zzjj.root 
