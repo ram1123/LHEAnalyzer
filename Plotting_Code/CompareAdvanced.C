c48c642ded7f8fefa28caa688617b96e7ae6280e
ram1123 <ramkrishna.sharma71@gmail.com>
2015-03-18 22:20:42 -0500

    Trying to add the Efficiency curve of cut in the code CompareAdvanced.C

--- Plotting_Code/CompareAdvanced.C
+++ Plotting_Code/CompareAdvanced.C
@@ -3,23 +3,23 @@ this program plots one branch of different trees on the same histogram
 
 var1 is the variable that we want to plot
 
-var2 is the xtitle
+var2: Presently not used
 
 n is the number of input rootfiles 
+
 
 format to enter
 
 	$root -l
 	root[].L CompareAdvanced.C
-	root[]compareQuantities("branch_name","x-axis Title",10,0,200,Cut,2,"abc.root","abc","xyz.root","xyz");
+	root[]compareQuantities( 1 , 0 , 0 , 0 , "branch_name", " " , "x-axis Title",10,0,200,Cut,2,"abc.root","abc","xyz.root","xyz");
 
 abc.root is the name of root file  --->>> abc is the legend of abc.root;   similarly others.
 */
 #include <string>
-#include "CrossSection.h"
+//#include "CrossSection.h"
 
-//double 13TeVScale(
-void compareQuantities(bool NormUnity, bool NormLumi, bool ShowEvents, string var1, string var2, string xtitle, int nbins, float min, float max, string cut="",int n,...){
+void compareQuantities(bool NormUnity, bool NormLumi, bool ShowEvents, bool GetFOMCut , string var1, string var2, string xtitle, int nbins, float min, float max, string cut="",int n,...){
 	
 	float a1=0.29;
 	float a2;
@@ -51,6 +51,16 @@ void compareQuantities(bool NormUnity, bool NormLumi, bool ShowEvents, string va
 	gROOT->ForceStyle(kTRUE);
 	TGaxis::SetMaxDigits(3);
 
+	#if 0
+	vector<int> VecA, VecB, VecC, VecD;
+		VecA.clear();
+		VecB.clear();
+		VecC.clear();
+		VecD.clear();
+	#else
+	float VecA[50] , VecB[50] , VecC[50] , VecD[50] , VecE[50];
+	#endif
+
 	va_list list;
 	va_start(list, n);
 	char** fname=new char*[n];
@@ -59,6 +69,7 @@ void compareQuantities(bool NormUnity, bool NormLumi, bool ShowEvents, string va
 	TH1F** th = new TH1F*[n];
 	TPaveStats** tp = new TPaveStats*[n];
 	TLegend** leg = new TLegend*[n];
+	TH1F * hs = new TH1F("hs","",nbins,min,max);
 
 	for (int i=0;i<n;i++)
 	{
@@ -67,9 +78,9 @@ void compareQuantities(bool NormUnity, bool NormLumi, bool ShowEvents, string va
 		fname[i]=va_arg(list, char*);
 		tf[i] = new TFile(fname[i]);
 		//tt[i] = (TTree*) tf[i]->Get("demo/tree");
-		tt[i] = (TTree*) tf[i]->Get("tree");
+		tt[i] = (TTree*) tf[i]->Get("tree");		// Put the name of tree here
 		th[i] = new TH1F(Form("th%i",i),"",nbins,min,max);
-		tt[i]->Draw(Form("%s>>th%i",var1.c_str(),i), cut.c_str(), "goff");
+		tt[i]->Draw(Form("%s>>th%i",var1.c_str(),i), cut.c_str());
 		
 		th[i]->SetStats(0);
 		th[i]->SetLineWidth(2);
@@ -111,7 +122,8 @@ void compareQuantities(bool NormUnity, bool NormLumi, bool ShowEvents, string va
 		th[i]->GetYaxis()->SetTitle("Normalized to Lumi");
 		}
 
-
+		if (i>0) hs->Add(th[i]);
+		if (i==0)TH1F *hist_new=(TH1F*)th[i]->Clone();
 		if (NormUnity)
 		{
 		if (i==0) th[i]->Scale(1./th[i]->Integral());
@@ -120,7 +132,43 @@ void compareQuantities(bool NormUnity, bool NormLumi, bool ShowEvents, string va
 		th[0]->SetMaximum(TMath::Max(th[i]->GetMaximum()*1.10,yMax));
 		yMax = TMath::Max(th[i]->GetMaximum()*1.10,yMax);
 		}
-		
+		if (GetFOMCut)
+		{
+		cout<<"##################################################################"<<endl;
+		for (int j = 1;  j<nbins; j++ )
+		{
+			//cout<<j<<"th\tBin Content = "<<th[i]->Integral(j)<<endl;
+			#if 1
+			//if (i==0) VecA.push_back(th[i]->Integral(j));
+			//if (i==1) VecB.push_back(th[i]->Integral(j));
+			//if (i==2) VecC.push_back(th[i]->Integral(j));
+			//if (i==3) VecD.push_back(th[i]->Integral(j));
+			if (i==0) {VecA[j]=th[i]->Integral(j,nbins)/th[i]->Integral();
+			VecE[j]=th[i]->GetBinCenter(j);
+
+			cout<<"VecA = "<<VecA[j]<<endl;
+			//cout<<"VecE = "<<VecE[j]<<endl;
+			}
+			if (i==1) {
+			VecB[j]=th[i]->Integral(j,nbins)/th[i]->Integral();
+			cout<<"VecB = "<<VecB[j]<<endl;
+			}
+			if (i==2) {
+			VecC[j]=th[i]->Integral(j,nbins)/th[i]->Integral();
+			cout<<"VecC = "<<VecC[j]<<endl;
+			}
+			if (i==3) {
+			VecD[j]=th[i]->Integral(j,nbins)/th[i]->Integral();
+			cout<<"VecD = "<<VecD[j]<<endl;
+			}
+			#else
+			//if(i==0) VecA[0]=th[i]->Integral();
+			//else VecB[0]+=th[i]->Integral();
+			cout<<j<<"\t"<<th[i]->Integral(j,nbins)<<endl;
+			#endif
+		}
+		}  // if (GetFOMCut)
+	
 		//th[0]->SetMaximum(0.0014);
 		if (i==0) th[i]->Draw(); else th[i]->Draw("sames");
 
@@ -155,6 +203,33 @@ void compareQuantities(bool NormUnity, bool NormLumi, bool ShowEvents, string va
 
 	}
 	va_end(list);
+
+
+hs->Scale(1/hs->Integral());
+hist_new->Scale(1/hist_new->Integral());
+hs->SetMaximum(0.050);
+hs->Draw();
+hist_new->Draw("same");
+
+for (int j = 1; j<nbins; j++)
+{
+VecA[j]=hs->Integral(j,nbins);
+VecB[j]=sqrt(hist_new->Integral(j,nbins));
+
+VecC[j]=VecA[j]/(VecB[j]);
+cout<<VecA[j]<<"\t"<<sqrt(VecB[j])<<"\t"<<VecB[j]<<endl;
+}			
+	gr = new TGraph(nbins,VecE,VecA);
+	gr2 = new TGraph(nbins,VecE,VecB);
+	gr3 = new TGraph(nbins,VecE,VecB);
+	gr->Draw("A*");
+	gr2->Draw("same");
+
+		if (GetFOMCut)
+		{
+	//cout<<"Sb = "<<VecA[0]/sqrt(VecB[0])<<endl;
+	//cout<<"signal = "<<VecA[0]<<"\tbkg = "<<VecB[0]<<endl;
+	}
 
 
 }
